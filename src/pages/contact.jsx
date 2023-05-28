65d72ca337fc522b3336771acb9b4c4d8f8e327d
casandrabenpor <casandra211@gmail.com>
2023-05-28 21:02:05 +0200

    customers2

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,10 @@
 import { Nav } from "../components/nav";
+import { TopBar } from "../components/topBar";
 import { Customers } from "../components/customers";
 export const Contact = () =>{
     return(
         <>
+        <TopBar/>
         <Nav/>
         <Customers showTitle={false} />
         </>

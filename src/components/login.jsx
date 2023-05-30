dcf3d1d741da28509e4c5e945683579b1fa712d2
casandrabenpor <casandra211@gmail.com>
2023-05-30 13:05:25 +0200

    btn login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,17 @@
 import logo from "../assets/logo.png";
-import { ContainerLogin , Form } from "../styled/loginStyled";
+import { ContainerLogin, Form } from "../styled/loginStyled";
 export const Login = () => {
     return (
         <ContainerLogin>
             <img src={logo} alt="logo" />
             <p>Hotel Miranda Admin Dashboard</p>
-            
+
             <Form>
                 <label htmlFor="email">Email</label>
                 <input type="text" id="email" name="email" />
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" name="password" />
-                <input type="submit" value="Login" />
+                <button type="submit" value="Login">Login</button>
             </Form>
         </ContainerLogin>
     )
--- src/styled/loginStyled.jsx
+++ src/styled/loginStyled.jsx
@@ -2,19 +2,50 @@ import styled from "styled-components";
 import { colors } from "./theme";
 
 export const ContainerLogin = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     color: ${colors.black};
     font-weight: 600;
-
+    border: none;
+    width: 50%;
+    background-color: rgb(255, 255, 255);
+    box-shadow: rgba(0, 0, 0, 0.08) 0px 16px 30px;
+    border-radius: 12px;
+    justify-content: center;
+   
   
 `;
+
 export const Form = styled.form`
 display: flex;
 flex-direction: column;
 align-items: center;
     color: ${colors.black};
+    button{
+        cursor: pointer;
+    border-radius: 12px;
+    border: none;
+    width: 215px;
+    height: 50px;
+    color: white;
+    background-color: ${colors.greenTwo};
+    margin-top: 15%;
+    font-family: "Poppins";
+    font-size: 16px;
+    }
+    input{
+        font-family: "Poppins";
+    border-radius: 12px;
+    border: none;
+    width: 220px;
+    height: 50px;
+    margin: 1em;
+    background-color: rgb(248, 248, 248);
+    color: ${colors.black};
+    box-shadow: rgba(0, 0, 0, 0.08) 0px 16px 30px;
+}
+    
 
   
 `;
